31ad4639a7541dd73a9f6b3782d530e04514bdd6
tania5496 <87234112+tania5496@users.noreply.github.com>
2023-01-05 21:35:26 +0200

    Update Main.java

--- src/main/java/Task1/Main.java
+++ src/main/java/Task1/Main.java
@@ -2,7 +2,7 @@ package Task1;
 
 public class Main {
     public static void main(String[] args)  {
-        User user = User.builder().name("Tetianka").age(18).gender(Gender.FEMALE).occupation("student").occupation("sport").build();
+        User user = User.builder().name("Tetianka").age(18).gender(Gender.FEMALE).occupation("student").build();
         System.out.println(user);
     }
 }
